31b77c1f3dafc80bb61cf70eaf0cc309ff2897e0
Quang Phu Ly <quangphuly150402@gmail.com>
2024-10-10 12:32:53 -0400

    Add GUI package

---
+++ src/GUI/MazeGUI.java
@@ -0,0 +1,9 @@
+package GUI;
+
+import java.io.File;
+
+public class MazeGUI {
+    public static final double MAZE_DEFAULT_PROPORTION = 0.50;
+    public static final double MAZE_PERISCOPE_PROPORTION = 0.75;
+    private static final File DATAFILE = new File("../datafile");
+}
